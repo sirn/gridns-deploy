f909e54fbaf1c5d06bdaf4bb6e243fb41e0f8217
Kridsada Thanabulpong <sirn@ogsite.net>
2019-12-05 22:58:08 +0900

    [chore] cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,47 +1,36 @@
 #!/bin/sh
+#
+# Bootstrap Ansible
+#
 
-_base_dir=$(cd "$(dirname "$0")/" || exit; pwd -P)
+BASE_DIR=$(cd "$(dirname "$0")/" || exit; pwd -P)
 
-_inventory_file="$_base_dir/hosts.yml"
-_requirements_file="$_base_dir/requirements.yml"
-_playbook_file="$_base_dir/playbook.yml"
+INVENTORY_FILE="$BASE_DIR/inventory.yml"
+REQUIREMENTS_FILE="$BASE_DIR/requirements.yml"
+PLAYBOOK_FILE="$BASE_DIR/playbook.yml"
 
+PLAYBOOK_BIN="ansible-playbook"
+GALAXY_BIN="ansible-galaxy"
 
-## Prepare
-##
-
-# FreeBSD uses `ansible-playbook-{PYVER}` for Python bins.
-for v in "" "-2.7" "-3.6" "-3.7"; do
-    if hash "ansible$v" 2>/dev/null; then
-        _ansible_playbook="ansible-playbook$v"
-        _ansible_galaxy="ansible-galaxy$v"
-        break
-    fi
-done
-
-if [ ! "$_ansible_playbook" ] || [ ! "$_ansible_galaxy" ]; then
-    printf "No known Ansible versions found.\\n"
+if [ ! "$PLAYBOOK_BIN" ] || [ ! "$GALAXY_BIN" ]; then
+    echo "No suitable Ansible versions found."
     exit 1
 fi
 
-if ! hash pass 2>/dev/null; then
-    printf "Pass is required to be installed.\\n"
-    exit 1
-fi
-
-case "$FORCE_UPDATE_GALAXY" in
+case "$UPDATE_GALAXY" in
     1 | y* | Y* | t* | T* )
-        _ansible_galaxy_args="--force";;
+        if ! "$GALAXY_BIN" install -f -r "$REQUIREMENTS_FILE"; then
+            printf "Cannot install Galaxy roles.\\n"
+            exit 1
+        fi
+        ;;
+
     * )
-        _ansible_galaxy_args="";;
+        ;;
 esac
 
-
-## Main
-##
-
-if ! "$_ansible_galaxy" install $_ansible_galaxy_args -r "$_requirements_file"; then
-    printf "Cannot install Galaxy roles.\\n"
+if ! command -v pass >/dev/null; then
+    printf "Pass is required to be installed.\\n"
     exit 1
 fi
 
@@ -52,11 +41,12 @@ trap 'rm -f $_fifo && rmdir $_temp' 0 1 2 3 6 14 15
 mkfifo "$_fifo"
 pass Ansible/gridns.xyz > "$_fifo" &
 
-env ANSIBLE_CONFIG="$_base_dir/ansible.cfg" \
-        "$_ansible_playbook" \
-        "$_playbook_file" \
-        -i "$_inventory_file" \
+exec \
+    env ANSIBLE_CONFIG="$BASE_DIR/ansible.cfg" \
+    "$PLAYBOOK_BIN" \
+        "$PLAYBOOK_FILE" \
         --user=freebsd \
+        --inventory "$INVENTORY_FILE" \
         --vault-password-file="$_fifo" \
-        --ssh-common-args="-o StrictHostKeyChecking=no" \
+        --ssh-common-args "-o StrictHostKeyChecking=no" \
         "$@"
